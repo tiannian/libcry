6ff8743418d69f4b32271e3316873fb47b0f240b
tiannian <dtiannian@aliyun.com>
2021-03-15 12:22:13 +0800

    add digest

--- src/primitive/digest.rs
+++ src/primitive/digest.rs
@@ -1,9 +1,1 @@
-// pub trait Digest {
-// const OUTPUT_SIZE: usize;
-//
-// fn new() -> Self;
-//
-// fn update(&self, bytes: &[u8]);
-//
-// fn finish(self) -> [u8; Self::OUTPUT_SIZE];
-// }
+pub use digest::Digest;
