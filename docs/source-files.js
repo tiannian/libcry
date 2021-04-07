f41660a1885d9409c44fd1b56813633a0523f3b8
tiannian <dtiannian@aliyun.com>
2021-04-07 15:25:06 +0800

    add new docs

---
+++ docs/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["cry"] = {"name":"","dirs":[{"name":"dh","files":["mod.rs"]},{"name":"keypair","files":["bare_public.rs","keypair.rs","mod.rs","public_key.rs"]},{"name":"primitive","files":["bytes.rs","digest.rs","mod.rs","point.rs","scalar.rs","sponge.rs"]},{"name":"ristretto255","files":["mod.rs","point.rs","scalar.rs"]},{"name":"schnorr","files":["mod.rs"]}],"files":["lib.rs"]};
+createSourceSidebar();
