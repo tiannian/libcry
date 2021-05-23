ef5947e3e71a0093a65ea3a874dc831fd9391ddc
tiannian <tiannian@users.noreply.github.com>
2021-05-23 17:00:19 +0000

    deploy: d415dbfdd74dd79ed70a699755b1afebb46425be

---
+++ implementors/cry/primitive/sponge/trait.Sponge.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["cry"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
