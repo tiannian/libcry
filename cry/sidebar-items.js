8fea08047f3c3bba69276925e5cb4d0c02942885
tiannian <tiannian@users.noreply.github.com>
2021-05-24 09:19:34 +0000

    deploy: 8f9f2dd8aab450944d72bc1e51062b3d7794cbbe

--- cry/sidebar-items.js
+++ cry/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["dh",""],["keccak","An implementation of the keccak-F[1600,800,400,200]."],["key",""],["primitive","Define primitives for cry."],["ristretto255",""],["schnorr","Schnorr signature and schnorr multi-signature."],["strobe",""]]});
+initSidebarItems({"mod":[["dh",""],["keccak","An implementation of the keccak-F[1600,800,400,200]."],["key","Define some type about asymmetric and symmetric key."],["primitive","Define primitives for cry."],["ristretto255",""],["schnorr","Schnorr signature and schnorr multi-signature."],["strobe",""]]});
